aaaee3bf43b51fd9cdd10d7c74c5ff33536feda2
simjeonghyeon1 <164852406+simjeonghyeon1@users.noreply.github.com>
2024-03-26 20:03:30 +0900

    Create HelloWorld.java

---
+++ 심정현/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+    public static void main(String[] args) { //psvm쓰고 tap을 누르면 메인함수가 자동으로 써짐
+        System.out.println("Hello World!"); //sout치고 tap치면 시스템 아웃 프린트인이 써짐
+    }
+}
