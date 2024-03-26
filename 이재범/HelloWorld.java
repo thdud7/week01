5bdb74c8f415f992fb6192f26c3eb3afd5b5f4ce
Beomjeja <151422133+Beomjeja@users.noreply.github.com>
2024-03-26 20:03:35 +0900

    Create HelloWorld.java

---
+++ 이재범/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) //psvm이라고 치면 기본 main 함수 나옴
+    {
+        System.out.println("Hello world"); //sout이라고 치면 System.out.println();나옴
+    }
+}
